6d46d0e73883624cdb68448091e192682d56c48c
Diogo Leal <diogolcsouza@gmail.com>
2022-01-15 15:22:58 -0300

    created simple api service

---
+++ src/main/java/general/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package general.employeemanager.repo;
+
+import general.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
